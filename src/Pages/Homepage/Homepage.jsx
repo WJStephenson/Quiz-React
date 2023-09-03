21a00c965a56b34c6160150dd4bf126e8913556a
WJStephenson <willjosteph@gmail.com>
2023-09-03 15:38:38 +0100

    added text links on homepage for easier mobile nav

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 //homepage with link to api documentation in new tab
 
+import { Link } from 'react-router-dom'
 import './Homepage.css'
 
 function Homepage() {
@@ -8,7 +9,7 @@ function Homepage() {
         <div className='homepage-wrapper'>
             <div className='homepage-container'>
                 <h1>A simple place to enjoy testing your knowledge.</h1>
-                <h3>Try our Quick Quiz to test your general knowledge, or Create a Quiz to pick a category.</h3>
+                <h3>Try our <Link to={`/quickquiz/any/10`}>Quick Quiz</Link> to test your general knowledge, or <Link to={'create'}>Create a Quiz</Link> to pick a category.</h3>
                 <p>Developed using the <a href='https://opentdb.com/' target='_blank'>Open Trivia Database</a></p>
             </div>
         </div>
